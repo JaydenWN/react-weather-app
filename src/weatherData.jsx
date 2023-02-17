0a75d6d906a51ee0b97699b7c11e22225b1eebb8
JaydenWN <123134488+JaydenWN@users.noreply.github.com>
2023-02-17 17:46:23 +1100

    added catch statement when calling api

--- src/weatherData.jsx
+++ src/weatherData.jsx
@@ -3,7 +3,7 @@ import { createContext } from "react"
 let weatherData = async ()=>{
     let weatherData = await fetch('https://api.open-meteo.com/v1/forecast?latitude=-33.42&longitude=151.37&hourly=temperature_2m&daily=weathercode,temperature_2m_max,temperature_2m_min&timezone=Australia%2FSydney')
     .then(response => response.json())
-    
+    .catch(res => console.log(`ERROR WITH API ${res}`))
     return weatherData
 }
 
