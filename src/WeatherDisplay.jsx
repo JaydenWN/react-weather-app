d0542c5bb3a42378ccf6d47dff6279817b0d8397
JaydenWN <123134488+JaydenWN@users.noreply.github.com>
2023-02-18 23:30:29 +1100

    added logic to convert api dates to day of the week

--- src/WeatherDisplay.jsx
+++ src/WeatherDisplay.jsx
@@ -4,6 +4,30 @@ import WeatherCard from "./weatherCard"
 
 let resource = dataFetch()
 
+function dayOfWeek(date){
+    let theDate = new Date(date)
+    let theDay = theDate.getDay()
+    
+    switch(theDay){
+        case 0:
+            return 'Sunday'
+        case 1:
+            return 'Monday'
+        case 2:
+            return 'Tuesday'
+        case 3:
+            return 'Wednesday'
+        case 4:
+            return 'Thursday'
+        case 5:
+            return 'Friday'
+        case 6:
+            return 'Saturday'
+        default:
+            return 'Something went wrong!'
+    }
+    return theDay
+}
 
 function WeatherDisplay(){
 
@@ -16,11 +40,12 @@ function WeatherDisplay(){
                 <WeatherCard 
                 maxTemp={weatherData.daily.temperature_2m_max[i]} 
                 minTemp={weatherData.daily.temperature_2m_min[i]}
-                date={weatherData.daily.time[i]}
-                unitType={weatherData.daily_units.temperature_2m_max}>
+                date={dayOfWeek(weatherData.daily.time[i])}
+                unitType={weatherData.daily_units.temperature_2m_max}
+                key={weatherData.daily.time[i]}>
                 </WeatherCard>
             )}{
-                console.log(weatherData)
+                
             }
         </Suspense>
      )
